af4897eecfe90806863e6625370d047cfd730aa4
Kedus-the-Great <114708517+Kedus-the-Great@users.noreply.github.com>
2023-09-19 16:50:26 -0400

    Uncommented intake

--- src/main/java/frc/robot/lib/interfaces/Intake/Intake.java
+++ src/main/java/frc/robot/lib/interfaces/Intake/Intake.java
@@ -13,22 +13,22 @@ public class Intake {
         this.io = io;
     }
     public void runIntake() {
-        // if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState) {
-        //     io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER);
-        // } else {
-        //     io.setPercentOutput(Constants.INTAKE.INTAKE_POWER);
-        // }
+        if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState) {
+            io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER);
+        } else {
+            io.setPercentOutput(Constants.INTAKE.INTAKE_POWER);
+        }
         io.setPercentOutput(0.5);
     }
 
     public void runOuttake() {
-        // if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState) {
-        //     io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER);
-        // } else if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState && RobotMap.elevatorStateMachine.getCurrentState() == ElevatorStateMachine.scoreMidState) {
-        //     io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER * 1.4);
-        // } else {
-        //     io.setPercentOutput(Constants.INTAKE.INTAKE_POWER);
-        // }
+        if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState) {
+            io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER);
+        } else if (RobotMap.ledStateMachine.getCurrentState() == LEDStateMachine.coneLEDState && RobotMap.elevatorStateMachine.getCurrentState() == ElevatorStateMachine.scoreMidState) {
+            io.setPercentOutput(-1 * Constants.INTAKE.INTAKE_POWER * 1.4);
+        } else {
+            io.setPercentOutput(Constants.INTAKE.INTAKE_POWER);
+        }
         io.setPercentOutput(0.5);
     }
 }
