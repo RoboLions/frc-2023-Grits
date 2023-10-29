78b0317f87039136d128e320e035a17cd3f01dbf
katiewang22 <66635481+katiewang22@users.noreply.github.com>
2023-10-29 15:37:01 -0400

    GRITS Day 2

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-masterchef-cooking";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 345;
-  public static final String GIT_SHA = "51039ab8db7a9800bea425aa7fda5309e19795a3";
-  public static final String GIT_DATE = "2023-10-24 19:38:43 EDT";
+  public static final int GIT_REVISION = 346;
+  public static final String GIT_SHA = "c5e4f679ef657e0b845be3eadd109663699beb69";
+  public static final String GIT_DATE = "2023-10-28 18:09:26 EDT";
   public static final String GIT_BRANCH = "Tentative-Autos";
-  public static final String BUILD_DATE = "2023-10-28 17:45:24 EDT";
-  public static final long BUILD_UNIX_TIME = 1698529524742L;
+  public static final String BUILD_DATE = "2023-10-29 14:51:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1698605487531L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
